fc047f9428519aa17fcd277b71c63c884edb2bc6
Andrey Sidorov <sidorares@yandex.ru>
2013-03-20 12:24:50 +1100

    initial import

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 yandex-translate
 ================
 
-Yandex.Translate translation service client
+[Yandex.Translate](http://api.yandex.com/translate/doc/dg/concepts/api-overview.xml) API client for node.js
+
+```js
+var translate = require('yandex-translate');
+translate('You can burn my house, steal my car, drink my liquor from an old fruitjar.' { to: 'ru' }, function(err, res) {
+  console.log(res.text);
+};
+
+translate.detect('Граждане Российской Федерации имеют право собираться мирно без оружия, проводить собрания, митинги и демонстрации, шествия и пикетирование', function(err, res) {
+   // res.lang -> 'ru'
+});
+```
+
+# License
+MIT. Yandex.Translate terms of service: http://legal.yandex.com/translate_api/
+
+# See also
+(Google Translate client)[https://github.com/Marak/translate.js] by @marak
+
---
+++ index.js
@@ -0,0 +1,83 @@
+var request = require('request');
+var endpoint = 'http://translate.yandex.net/api/v1/tr.json';
+
+var jsonRequest = function(url, params, cb) {
+  var handler = function(err, res) {
+    if (err)
+      return cb(err);
+    var obj;
+    try {
+      obj = JSON.parse(res.body);
+    } catch(e) {
+      cb(e);
+    }
+    cb(null, obj);
+  };
+  if (params.get === true)
+    request.get(url, handler);
+  else
+    request.post(url, params, handler);
+};
+
+var translate  = function(text, opts, cb)
+{
+  var topts = typeof opts;
+  if (topts == 'function' || topts == 'undefined') {
+    if (topts == 'function')
+      cb = opts;
+    opts = {
+      to: 'en',
+      format: 'text'
+    };
+  }
+  if (!opts.to)
+    opts.to = 'en';
+  if (!opts.format)
+    opts.format = 'text';
+  jsonRequest(endpoint + '/translate', {
+      form: {
+        text: text,
+        format: opts.format,
+        lang: opts.from ? opts.from + '-' + opts.to : opts.to
+      }
+  }, cb);
+};
+
+var getLanguages = function(cb) {
+  jsonRequest(endpoint + '/getLangs', { get: true }, cb);
+};
+
+var detect = function(text, opts, cb) {
+  var topts = typeof opts;
+  if (topts == 'function' || topts == 'undefined') {
+    if (topts == 'function')
+      cb = opts;
+    opts = {
+      format: 'text'
+    };
+  }
+  if (!opts.format)
+    opts.format = 'text';
+  jsonRequest(endpoint + '/detect', {
+        form: {
+          text: text,
+          format: opts.format
+        }
+     }, cb);
+};
+
+
+module.exports = translate;
+module.exports.translate = translate; // make translate 'default' exported function
+module.exports.detect = detect;
+module.exports.getLangs = getLanguages;
+
+// simple inline test
+if (require.main === module) {
+  translate('Граждане Российской Федерации имеют право собираться мирно без оружия, проводить собрания, митинги и демонстрации, шествия и пикетирование', function(err, res) {
+    console.assert(err === null, "Got transport level errors");
+    console.assert(res.code === 200, "Non 200 HTTP code");
+    console.assert(res.lang === "ru-en", "Language autodetected incorrectly");
+    console.log(res.text.join());
+  });
+}
